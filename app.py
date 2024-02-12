d9fa6fcb69e9ba9c53b040a2b0ea393c013a9164
Brien123 <zbrine1335@gmail.com>
2024-02-12 14:01:12 +0100

    Made the code readable

--- app.py
+++ app.py
@@ -6,13 +6,22 @@ from IPython.display import Markdown
 import re
 import random
 import json
-from functions.function import score, replace_numbers_with_random, generate_modified_content
+from functions.function import score, replace_numbers_with_random, generate_modified_content, answer_questions
 
 app = Flask(__name__)
 
 # Endpoint to receive POST requests containing data
 @app.route('/calculate_score', methods=['POST'])
 def calculate_score():
+    """
+Example df: {
+    "Topics": ["Math", "Math", "Physics", "Physic", "Chemistry"],
+    "Correctness": [1, 0, 1, 1, 0],
+    "Difficulty": [1, 3, 2, 2, 3],
+    "Time Spent": [30, 40, 20, 25, 35],
+    "Number of Attempts": [3, 4, 2, 5, 3]
+}
+"""
     try:
         # Get the JSON data from the POST request
         data = request.get_json()
@@ -36,6 +45,11 @@ def calculate_score():
 # route for generating new questions from old
 @app.route('/paraphrase', methods=['POST'])
 def paraphrase_question():
+    """
+    Example question: {
+    "question": "A boy walks a distance of 5 km in 100 minutes, what is his speed. what is the difference between speed and velocity"
+    }
+    """
     if request.method == 'POST':
         try:
             # Getting the question from the POST request
@@ -51,19 +65,14 @@ def paraphrase_question():
             return jsonify({'error': str(e)})
 
 
-def answer_questions(question):
-
-    # Answering the questions
-    model = genai.GenerativeModel('gemini-pro')
-    prefix = "Please answer question: "
-    response = model.generate_content(prefix + question)
-
-    return response.text
-
-
-# route for generating new questions from old
+# route for answering new questions from old
 @app.route('/answer', methods=['POST'])
 def answers():
+        """
+    Example question: {
+    "question": "A boy walks a distance of 5 km in 100 minutes, what is his speed. what is the difference between speed and velocity"
+    }
+    """
     if request.method == 'POST':
         try:
             # Getting the question from the POST requests
--- functions/function.py
+++ functions/function.py
@@ -11,6 +11,16 @@ GOOGLE_API_KEY= "AIzaSyBgc-O6x9qII5OpTGIyxYrHyICAdRj-Pe0"
 genai.configure(api_key = GOOGLE_API_KEY)
 
 def score(df):
+    """
+    Calculates the proguide score of a given student for a particular Q/A session.
+
+    Parameters:
+        df (dict): A dictionary containing the various scores for the questions answered
+
+    Returns:
+        int: ProGuide score
+    """
+        
     # Maximum values for each column
     numerical_columns = ['Correctness', 'Difficulty', 'Time Spent']
     max_values = df[numerical_columns].max()
@@ -69,7 +79,7 @@ def generate_modified_content(question, percentage_range=25):
         percentage_range (int): The percentage range for random value generation. Default is 25.
 
     Returns:
-        str: The modified content with numerical values replaced.
+        str: Generated question
     """
 
     GOOGLE_API_KEY= "AIzaSyBgc-O6x9qII5OpTGIyxYrHyICAdRj-Pe0" #API key i got from google developer site. it needs to be kept in a .env file. This will be done in further updates
@@ -81,5 +91,24 @@ def generate_modified_content(question, percentage_range=25):
     model = genai.GenerativeModel('gemini-pro')
     prefix = "Use this question to form a short physics question: "
     response = model.generate_content(prefix + modified_question)
+
+    return response.text
+
+
+def answer_questions(question):
+    """
+    Answer questions posed to it.
+
+    Parameters:
+        question (str): The input question containing numerical values to be replaced.
+
+    Returns:
+        str: Answer to the question
+    """
+
+    # Answering the questions
+    model = genai.GenerativeModel('gemini-pro')
+    prefix = "Please answer question: "
+    response = model.generate_content(prefix + question)
 
     return response.text
